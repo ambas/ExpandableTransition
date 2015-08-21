8e82c6523b7deaa2c5400584639ccfbc4a1a532b
Ambas Mac Air <ambas.c@taskworld.com>
2015-08-21 22:24:20 +0700

    Add Plist file reader

---
+++ Demo/Demo/PlistReader.swift
@@ -0,0 +1,25 @@
+//
+//  PlistReader.swift
+//  Demo
+//
+//  Created by Ambas Chobsanti on 8/21/2558 BE.
+//  Copyright (c) 2558 Taskworld. All rights reserved.
+//
+
+import UIKit
+
+class PlistReader {
+    var fileName : String
+    var filePath
+    
+    init(fileName : String) {
+        self.fileName = fileName
+    }
+    
+    func arrayFromFile -> Array {
+        
+    }
+    
+    
+    
+}
